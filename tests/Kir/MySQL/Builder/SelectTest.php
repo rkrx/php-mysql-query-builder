d86671e4f8bc29cf6f3cd6f8ac98addcea1cc481
ron <ron.kirschler@gmail.com>
2014-05-06 22:47:57 +0200

    Initial import

--- tests/Kir/MySQL/Builder/SelectTest.php
+++ tests/Kir/MySQL/Builder/SelectTest.php
@@ -55,6 +55,20 @@ class SelectTest extends \PHPUnit_Framework_TestCase {
 		->where('a+1<2')
 		->asString();
 		$this->assertEquals('SELECT a FROM test t WHERE a+1<2 ;', $str);
+
+		$str = TestSelect::create()
+		->field('a')
+		->from('t', 'test')
+		->where('a < ?', 1000)
+		->asString();
+		$this->assertEquals('SELECT a FROM test t WHERE a < 1000 ;', $str);
+
+		$str = TestSelect::create()
+		->field('a')
+		->from('t', 'test')
+		->where('a < :0', 1000)
+		->asString();
+		$this->assertEquals('SELECT a FROM test t WHERE a < 1000 ;', $str);
 	}
 
 	public function testHaving() {
