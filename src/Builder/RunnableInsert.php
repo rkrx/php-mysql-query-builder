474132ea22c55bdce13686ecebad92bf7e6624ec
ron <ron.kirschler@gmail.com>
2015-04-16 11:00:11 +0200

    Added RunnableInsert::insertRows to add a huge amount of rows utilizing prepared statements. Method returns all insert ids.

--- src/Builder/RunnableInsert.php
+++ src/Builder/RunnableInsert.php
@@ -3,6 +3,21 @@ namespace Kir\MySQL\Builder;
 
 class RunnableInsert extends Insert {
 	/**
+	 * @param array $rows
+	 * @return int[] Insert IDs
+	 */
+	public function insertRows(array $rows) {
+		$result = [];
+		$query = $this->__toString();
+		$stmt = $this->db()->prepare($query);
+		foreach($rows as $row) {
+			$stmt->execute($row);
+			$result[] = (int) $this->db()->getLastInsertId();
+		}
+		return $result;
+	}
+
+	/**
 	 */
 	public function run() {
 		$query = $this->__toString();
